6120ff3a416ea34fb49277b160d94c3d77ec8ab1
OUALIID <walidelhadim8@gmail.com>
2024-04-02 23:32:53 +0000

    Add JavaScript code to handle puzzle pieces, their animations, and shuffling logic

--- static/scripts/jigsawGameController.js
+++ static/scripts/jigsawGameController.js
@@ -125,6 +125,46 @@ const startGame = async () => {
     chooseImageLabel.classList.add("notShown", "pointer-events-none");
     orText.classList.add("notShown");
     /* pointer-events-none */
-    
+
+
+      // const pieces = [1, 2, 3, 4, 5];
+  // Array(pieces.length) We create a new array with length equal to the number of puzzle pieces,
+  // .fill(false) We fill each element of the array with the value `false`.like [false, false ...]
+  const PieceCompleted = Array(pieces.length).fill(false);
+
+  let PieceBeingDragged = false; // Indicates whether a puzzle piece is currently being dragged by the user
+  let zIndex = pieces.length;
+
+  // Loop through each puzzle piece and create an image element for it.
+  // The "puzzlePiece" represents an individual piece of the puzzle.
+  // "pieceIndex" helps us keep track of the order of pieces.
+  pieces.forEach((puzzlePiece, pieceIndex) => {
+    const pieceImageElement = new Image();
+    // Define a function to animate the puzzle piece
+    pieceImageElement.animatePuzzlePiece = () => {
+      pieceImageElement.style.transition = "all 0.5s ease-in-out";
+      // Check if shuffling is enabled
+      if (shuffleTiles) {
+        const randomIndex = getRandomNumber(shufflePositions.length);
+        pieceImageElement.style.left = `${shufflePositions[randomIndex].shuffleX(board, tile)}px`;
+        pieceImageElement.style.top = `${shufflePositions[randomIndex].shuffleY(board, tile)}px`;
+        pieceImageElement.style.zIndex = getRandomNumber(pieces.length) + 1;
+      } else {
+        // If shuffling is disabled, set the piece to its original position
+        pieceImageElement.style.left = `${puzzlePiece.leftOffset}px`;
+        pieceImageElement.style.top = `${puzzlePiece.topOffset}px`;
+      }
+      // Remove transition effect after 500 milliseconds
+      setTimeout(() => (pieceImageElement.style.transition = "none"), 500);
+      PieceCompleted.fill(false);
+    };
+
+    // Set the source and class of the puzzle piece image
+    pieceImageElement.src = puzzlePiece.base64Url;
+    pieceImageElement.className = "piece";
+    // Append the puzzle piece image to the puzzle container
+    puzzleContainerElement.appendChild(pieceImageElement);
+    pieceImageElement.animatePuzzlePiece();
+
 };
 
